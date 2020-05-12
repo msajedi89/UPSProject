88c504082df59df5c68bc51370c1efa7b3a075ee
msajedi89 <m.sajedi89@gmail.com>
2020-05-12 14:14:52 -0700

    edit 2 in 5-12

--- GetUPSRates.php
+++ GetUPSRates.php
@@ -3,12 +3,90 @@
 
 class GetUPSRates
 {
+    // Shipper Properties
+    private $shipperName;
+    private $shipperNumber = "";
+    private $shipperCity;
+    private $shipperStateProvinceCode;
+    private $shipperPostalCode;
+    private $shipperCountryCode;
+
+    // ShipTo Properties
+    private $shipToName;
+    private $shipToCity;
+    private $shipToStateProvinceCode;
+    private $shipToPostalCode;
+    private $shipToCountryCode;
+
     private $packageWeight;
     private $packageHeight;
     private $packageWidth;
     private $packageLength;
 
-    private $shipperName;
+
+    // ******* SHIPPER *******
+
+    // set Shipper Name
+    function setShipperName($shipperName) {
+        $this->shipperName = $shipperName;
+    }
+
+    // set Shipper Number
+    function setShipperNumber($shipperNumber) {
+        $this->shipperNumber = $shipperNumber;
+    }
+
+    // set Shipper City
+    function setShipperCity($shipperCity) {
+        $this->shipperCity = $shipperCity;
+    }
+
+    // set Shipper StateProvinceCode
+    function setShipperStateProvinceCode($shipperStateProvinceCode) {
+        $this->shipperStateProvinceCode = $shipperStateProvinceCode;
+    }
+
+    // set Shipper PostalCode
+    function setShipperPostalCode($shipperPostalCode) {
+        $this->shipperPostalCode = $shipperPostalCode;
+    }
+
+    // set Shipper CountryCode
+    function setShipperCountryCode($shipperCountryCode) {
+        $this->shipperCountryCode = $shipperCountryCode;
+    }
+
+
+    // ******* SHIP TO *******
+
+    // set ShipTo Name
+    function setShipToName($shipToName) {
+        $this->shipToName = $shipToName;
+    }
+
+    // set ShipTo City
+    function setShipToCity($shipToCity) {
+        $this->shipToCity = $shipToCity;
+    }
+
+    // set ShipTo StateProvinceCode
+    function setShipToStateProvinceCode($shipToStateProvinceCode) {
+        $this->shipToStateProvinceCode = $shipToStateProvinceCode;
+    }
+
+    // set ShipTo PostalCode
+    function setShipToPostalCode($shipToPostalCode) {
+        $this->shipToPostalCode = $shipToPostalCode;
+    }
+
+    // set ShipTo CountryCode
+    function setShipToCountryCode($shipToCountryCode) {
+        $this->shipToCountryCode = $shipToCountryCode;
+    }
+
+
+
+
 
     // set Package Weight
     function setPackageWeight($weight) {
@@ -30,10 +108,7 @@ class GetUPSRates
         $this->packageLength = $length;
     }
 
-    // set Shipper Name
-    function setShipperName($shipperName) {
-        $this->shipperName = $shipperName;
-    }
+
 
     // Get the Rates for Package
     function processRate() {
@@ -48,7 +123,7 @@ class GetUPSRates
             CURLOPT_FOLLOWLOCATION => true,
             CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
             CURLOPT_CUSTOMREQUEST => "POST",
-            CURLOPT_POSTFIELDS =>"{\r\n   \"RateRequest\":{\r\n      \"Request\":{\r\n         \"SubVersion\":\"1703\",\r\n         \"TransactionReference\":{\r\n            \"CustomerContext\":\" \"\r\n         }\r\n      },\r\n      \"Shipment\":{\r\n         \"ShipmentRatingOptions\":{\r\n            \"UserLevelDiscountIndicator\":\"TRUE\"\r\n         },\r\n         \"Shipper\":{\r\n            \"Name\":\"$this->shipperName\",\r\n            \"ShipperNumber\":\" \",\r\n            \"Address\":{\r\n               \"City\":\"Marietta\",\r\n               \"StateProvinceCode\":\"GA\",\r\n               \"PostalCode\":\"30067\",\r\n               \"CountryCode\":\"US\"\r\n            }\r\n         },\r\n         \"ShipTo\":{\r\n            \"Name\":\"Sarita Lynn\",\r\n            \"Address\":{\r\n               \"City\":\"San Jose\",\r\n               \"StateProvinceCode\":\"CA\",\r\n               \"PostalCode\":\"95113\",\r\n               \"CountryCode\":\"US\"\r\n            }\r\n         },\r\n         \"ShipFrom\":{\r\n            \"Name\":\"Billy Blanks\",\r\n            \"Address\":{\r\n               \"AddressLine\":\"366 Robin LN SE\",\r\n               \"City\":\"Marietta\",\r\n               \"StateProvinceCode\":\"GA\",\r\n               \"PostalCode\":\"30067\",\r\n               \"CountryCode\":\"US\"\r\n            }\r\n         },\r\n         \"Service\":{\r\n            \"Code\":\"03\",\r\n            \"Description\":\"Ground\"\r\n         },\r\n         \"ShipmentTotalWeight\":{\r\n            \"UnitOfMeasurement\":{\r\n               \"Code\":\"LBS\",\r\n               \"Description\":\"Pounds\"\r\n            },\r\n            \"Weight\":\"10\"\r\n         },\r\n         \"Package\":{\r\n            \"PackagingType\":{\r\n               \"Code\":\"02\",\r\n               \"Description\":\"Package\"\r\n            },\r\n            \"Dimensions\":{\r\n               \"UnitOfMeasurement\":{\r\n                  \"Code\":\"IN\"\r\n               },\r\n               \"Length\":\"$this->packageLength\",\r\n               \"Width\":\"$this->packageWidth\",\r\n               \"Height\":\"$this->packageHeight\"\r\n            },\r\n            \"PackageWeight\":{\r\n               \"UnitOfMeasurement\":{\r\n                  \"Code\":\"LBS\"\r\n               },\r\n               \"Weight\":\"$this->packageWeight\"\r\n            }\r\n         }\r\n      }\r\n   }\r\n}",
+            CURLOPT_POSTFIELDS =>"{\r\n   \"RateRequest\":{\r\n      \"Request\":{\r\n         \"SubVersion\":\"1703\",\r\n         \"TransactionReference\":{\r\n            \"CustomerContext\":\" \"\r\n         }\r\n      },\r\n      \"Shipment\":{\r\n         \"ShipmentRatingOptions\":{\r\n            \"UserLevelDiscountIndicator\":\"TRUE\"\r\n         },\r\n         \"Shipper\":{\r\n            \"Name\":\"$this->shipperName\",\r\n            \"ShipperNumber\":\"$this->shipperNumber\",\r\n            \"Address\":{\r\n               \"City\":\"$this->shipperCity\",\r\n               \"StateProvinceCode\":\"$this->shipperStateProvinceCode\",\r\n               \"PostalCode\":\"$this->shipperPostalCode\",\r\n               \"CountryCode\":\"$this->shipperCountryCode\"\r\n            }\r\n         },\r\n         \"ShipTo\":{\r\n            \"Name\":\"$this->shipToName\",\r\n            \"Address\":{\r\n               \"City\":\"$this->shipToCity\",\r\n               \"StateProvinceCode\":\"$this->shipToStateProvinceCode\",\r\n               \"PostalCode\":\"$this->shipToPostalCode\",\r\n               \"CountryCode\":\"$this->shipToCountryCode\"\r\n            }\r\n         },\r\n         \"ShipFrom\":{\r\n            \"Name\":\"Billy Blanks\",\r\n            \"Address\":{\r\n               \"AddressLine\":\"366 Robin LN SE\",\r\n               \"City\":\"Marietta\",\r\n               \"StateProvinceCode\":\"GA\",\r\n               \"PostalCode\":\"30067\",\r\n               \"CountryCode\":\"US\"\r\n            }\r\n         },\r\n         \"Service\":{\r\n            \"Code\":\"03\",\r\n            \"Description\":\"Ground\"\r\n         },\r\n         \"ShipmentTotalWeight\":{\r\n            \"UnitOfMeasurement\":{\r\n               \"Code\":\"LBS\",\r\n               \"Description\":\"Pounds\"\r\n            },\r\n            \"Weight\":\"10\"\r\n         },\r\n         \"Package\":{\r\n            \"PackagingType\":{\r\n               \"Code\":\"02\",\r\n               \"Description\":\"Package\"\r\n            },\r\n            \"Dimensions\":{\r\n               \"UnitOfMeasurement\":{\r\n                  \"Code\":\"IN\"\r\n               },\r\n               \"Length\":\"$this->packageLength\",\r\n               \"Width\":\"$this->packageWidth\",\r\n               \"Height\":\"$this->packageHeight\"\r\n            },\r\n            \"PackageWeight\":{\r\n               \"UnitOfMeasurement\":{\r\n                  \"Code\":\"LBS\"\r\n               },\r\n               \"Weight\":\"$this->packageWeight\"\r\n            }\r\n         }\r\n      }\r\n   }\r\n}",
             CURLOPT_HTTPHEADER => array(
                 "Username: DAN_Kashani",
                 "Password: Shelly12",
@@ -60,7 +135,7 @@ class GetUPSRates
         $response = curl_exec($curl);
         curl_close($curl);
 
-        echo $response;
+        return $response;
     }
 
 }
--- callUPSclass.php
+++ callUPSclass.php
@@ -4,12 +4,29 @@ include 'GetUPSRates.php';
 
 $ups = new GetUPSRates();
 
+// Provide Shipper Info
+$ups->setShipperName('Mansoor');
+$ups->setShipperNumber('');
+$ups->setShipperCity('Marietta');
+$ups->setShipperStateProvinceCode('GA');
+$ups->setShipperPostalCode('30067');
+$ups->setShipperCountryCode('US');
+
+// Provide ShipTo Info
+$ups->setShipToName('Mansoor');
+$ups->setShipToCity('Marietta');
+$ups->setShipToStateProvinceCode('GA');
+$ups->setShipToPostalCode('30067');
+$ups->setShipToCountryCode('US');
+
+
+// Set Package Info
 $ups->setPackageHeight('10');
 $ups->setPackageWidth('8');
 $ups->setPackageLength('7');
 $ups->setPackageWeight('15');
-$ups->setShipperName('Mansoor');
-
-$ups->processRate();
 
 
+$result = $ups->processRate();
+
+echo $result;
